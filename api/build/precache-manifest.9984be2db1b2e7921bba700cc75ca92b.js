286b8eda14b2e43987de484dfc2523cf2e80a2cf
ajmcquilkin <46639306+ajmcquilkin@users.noreply.github.com>
2020-03-17 20:49:47 -0400

    Working serving of react on node/express

---
+++ api/build/precache-manifest.9984be2db1b2e7921bba700cc75ca92b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "777b4cebaa9f602b6b6f752c27e29243",
+    "url": "/index.html"
+  },
+  {
+    "revision": "786174a46647b4ff53c5",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e6967729dcb953f9720a",
+    "url": "/static/js/2.6388cf68.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.6388cf68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "786174a46647b4ff53c5",
+    "url": "/static/js/main.17b9fa03.chunk.js"
+  },
+  {
+    "revision": "3819bbd6a0325a49e6bb",
+    "url": "/static/js/runtime-main.c03bff9b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- api/server.js
+++ api/server.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var path = require('path');
+var serveStatic = require('serve-static');
 
 const app = express();
 
+// app.use(serveStatic('/build', { 'index': 'index.html' }));
+app.use(express.static(path.join(__dirname, 'build')));
+
 app.get('/', (req, res) => {
   res.send('Hello!');
+});
+
+app.get('/react', (req, res) => {
+  res.setHeader('Content-Type', 'text/html');
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  console.log(res);
 });
 
 app.listen(8080);
